44579b5ca5756d8cd614f674e6eb67661aa8297c
unknown <olbidojunex@gmail.com>
2026-04-11 13:11:48 +0800

    final na ata ni

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    preview: {
+        // This tells Vite 7 to trust Render's URL
+        allowedHosts: ['goldtown-inventory.onrender.com']
+    }
 });
